63aca5c40aa53b8a92c425b7b896755f230b54fd
Timothy Garcia <timothyiangarcia@gmail.com>
2021-03-20 15:43:20 -0400

    added save and load player rotation

--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -108,6 +108,7 @@ public class ControlPanelController : MonoBehaviour
     {
         player.controller.enabled = false;
         player.transform.position = sceneData.playerPosition;
+        player.transform.rotation = sceneData.playerRotation;
         player.controller.enabled = true;
 
         player.health = sceneData.playerHealth;
